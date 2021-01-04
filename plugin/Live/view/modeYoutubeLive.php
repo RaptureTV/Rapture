175ad729e28f36f571b2bf5a0424ce74035e1986
faizy <faizymughal52@gmail.com>
2021-01-04 22:28:50 +0500

    change position of chatbox

--- plugin/Live/view/modeYoutubeLive.php
+++ plugin/Live/view/modeYoutubeLive.php
@@ -137,11 +137,18 @@ if(empty($sideAd) && !AVideoPlugin::loadPluginIfEnabled("Chat2")){
                                         <?php echo getAdsLeaderBoardTop(); ?>
                                     </center>
                                 </div>  
-                                <div class="col-md-12">
+                                <div class="col-md-8">
                                     <?php
                                     require "{$global['systemRootPath']}plugin/Live/view/liveVideo.php";
                                     ?>
                                 </div>  
+                                <div class="col-sm-4 col-md-4 col-lg-4 rightBar" id="yptRightBar">
+                        <div class="list-group-item ">
+                            <?php
+                            echo $sideAd;
+                            ?>
+                        </div>
+                    </div>
                                 <div class="col-md-12">
                                     <center style="margin:5px;">
                                         <?php echo getAdsLeaderBoardTop2(); ?>
