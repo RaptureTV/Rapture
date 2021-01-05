be4fea446ceb01488d33fe92372172c2819c00c2
Usman Asif <usman.asif.zai@gmail.com>
2021-01-05 11:32:52 -0800

    updated two files

--- plugin/Live/view/modeYoutubeLive.php
+++ plugin/Live/view/modeYoutubeLive.php
@@ -45,7 +45,9 @@ if(!empty($_REQUEST['playlists_id_live'])){
 }
 
 
-$video['creator'] = '<div class="pull-left"><img src="' . $liveImg . '" alt="User Photo" class="img img-responsive img-circle" style="max-width: 40px;"/></div><div class="commentDetails" style="margin-left:45px;"><div class="commenterName text-muted"><strong>' . $name . '</strong><br>' . $subscribe . '</div></div>';
+$video['creator'] = '<div class="pull-left"><img src="' . $liveImg . '" alt="User Photo" class="img img-responsive img-circle" style="max-width: 40px;"/></div><div class="commentDetails" style="margin-left:45px;"><div class="commenterName text-muted"><strong>' . $name . '</strong><br></div></div>';
+$video['creator1'] = '<div class="commentDetails" style="margin-left:45px;"><div class="commenterName text-muted"><strong></strong><br>' . $subscribe . '</div></div>';
+$video['creator2'] = '<div class="commentDetails" style="margin-left:45px;"><div class="commenterName text-muted"><strong></strong><br>' . $subscribe . '</div></div>';
 
 $img = "{$global['webSiteRootURL']}plugin/Live/getImage.php?u={$_GET['u']}&format=jpg";
 $imgw = 640;
@@ -59,8 +61,8 @@ $isCompressed = AVideoPlugin::loadPluginIfEnabled('TheaterButton') && TheaterBut
 
 $sideAd = getAdsSideRectangle();
 
-$modeYoutubeBottomClass1 = "col-sm-7 col-md-7 col-lg-6";
-$modeYoutubeBottomClass2 = "col-sm-5 col-md-5 col-lg-4 ";
+$modeYoutubeBottomClass1 = "col-sm-7 col-md-8 col-lg-9";
+$modeYoutubeBottomClass2 = "col-sm-4 col-md-3 col-lg-3 ";
 if(empty($sideAd) && !AVideoPlugin::loadPluginIfEnabled("Chat2")){
     $modeYoutubeBottomClass1 = "col-sm-12 col-md-12 col-lg-10";
     $modeYoutubeBottomClass2 = "hidden ";
@@ -88,6 +90,14 @@ if(empty($sideAd) && !AVideoPlugin::loadPluginIfEnabled("Chat2")){
         <?php
         //echo AVideoPlugin::getHeadCode();
         ?>
+        <style>
+        .container-fluid{
+          background-color: #000000 !important;
+        }
+        .list-group-item{
+            display:none !important;
+        }
+        </style>
     </head>
 
     <body class="<?php echo $global['bodyClass']; ?>">
@@ -105,9 +115,66 @@ if(empty($sideAd) && !AVideoPlugin::loadPluginIfEnabled("Chat2")){
                             </center>
                         </div>
                         <div class="col-md-12">
+                        <?php
+$_REQUEST['live_servers_id'] = Live::getLiveServersIdRequest();
+$poster = Live::getPosterImage($livet['users_id'], $_REQUEST['live_servers_id']);
+?>
+<link href="<?php echo $global['webSiteRootURL']; ?>plugin/Live/view/live.css" rel="stylesheet" type="text/css"/>
+<div class="row main-video" id="mvideo">
+    <div class="secC col-sm-8 col-md-9" style="padding-right:1px;padding-left:1px;">
+        <div id="videoContainer">
+            <div id="floatButtons" style="display: none;">
+                <p class="btn btn-outline btn-xs move">
+                    <i class="fas fa-expand-arrows-alt"></i>
+                </p>
+                <button type="button" class="btn btn-outline btn-xs" onclick="closeFloatVideo();floatClosed = 1;">
+                    <i class="fas fa-times"></i>
+                </button>
+            </div>
+            <div id="main-video" class="embed-responsive embed-responsive-16by9">
+                <video poster="<?php echo $global['webSiteRootURL']; ?><?php echo $poster; ?>?<?php echo filectime($global['systemRootPath'] . $poster); ?>" controls playsinline webkit-playsinline="webkit-playsinline"
+                       class="embed-responsive-item video-js vjs-default-skin vjs-big-play-centered liveVideo vjs-16-9"
+                       id="mainVideo">
+                    <source src="<?php echo Live::getM3U8File($uuid); ?>" type='application/x-mpegURL'>
+                </video>
+                <?php
+                if (AVideoPlugin::isEnabled("0e225f8e-15e2-43d4-8ff7-0cb07c2a2b3b")) {
+                    require_once $global['systemRootPath'] . 'plugin/VideoLogoOverlay/VideoLogoOverlay.php';
+                    $style = VideoLogoOverlay::getStyle();
+                    $url = VideoLogoOverlay::getLink();
+                    ?>
+                    <div style="<?php echo $style; ?>">
+                        <a href="<?php echo $url; ?>" target="_blank"> <img src="<?php echo $global['webSiteRootURL']; ?>videos/logoOverlay.png" alt="Logo" class="img-responsive col-lg-12 col-md-8 col-sm-7 col-xs-6"></a>
+                    </div>
+                <?php } ?>
+
+
+            </div>
+            <div style="z-index: 999; position: absolute; top:5px; left: 5px; opacity: 0.8; filter: alpha(opacity=80);">
+                <?php
+                $streamName = $uuid;
+                include $global['systemRootPath'] . 'plugin/Live/view/onlineLabel.php';
+                include $global['systemRootPath'] . 'plugin/Live/view/onlineUsers.php';
+                ?>
+            </div>
+        </div>
+    </div>
+    <div class="<?php echo $modeYoutubeBottomClass2; ?> rightBar" id="yptRightBar">
+                        <div class="list-group-item ">
                             <?php
-                            require "{$global['systemRootPath']}plugin/Live/view/liveVideo.php";
+                            echo $sideAd;
                             ?>
+                        </div>
+                    </div>
+</div>
+<script>
+<?php
+//comment
+echo PlayerSkins::getStartPlayerJS();
+?>
+</script>
+
+
                         </div>
                         <div class="col-md-12">
                             <center style="margin:5px;">
@@ -119,14 +186,13 @@ if(empty($sideAd) && !AVideoPlugin::loadPluginIfEnabled("Chat2")){
                 }
                 ?>
                 <div class="row" id="modeYoutubeBottom" style="margin: 0;">
-                    <div class="col-lg-1"></div>
                     <div class="<?php echo $modeYoutubeBottomClass1; ?>" id="modeYoutubeBottomContent">
                         <?php
                         if ($isCompressed) {
                             ?>
                             <div class="" id="modeYoutubeTop" >
                                 <div class="col-md-12">
-                                    <center style="margin:5px;">
+                                    <center >
                                         <?php echo getAdsLeaderBoardTop(); ?>
                                     </center>
                                 </div>
@@ -136,7 +202,7 @@ if(empty($sideAd) && !AVideoPlugin::loadPluginIfEnabled("Chat2")){
                                     ?>
                                 </div>
                                 <div class="col-md-12">
-                                    <center style="margin:5px;">
+                                    <center>
                                         <?php echo getAdsLeaderBoardTop2(); ?>
                                     </center>
                                 </div>
@@ -144,7 +210,8 @@ if(empty($sideAd) && !AVideoPlugin::loadPluginIfEnabled("Chat2")){
                             <?php
                         }
                         ?>
-                        <div class="panel">
+
+                        <div class="panel" style="background-color: #000000 !important;">
                             <div class="panel-body">
                                 <h1 itemprop="name">
                                     <?php
@@ -161,7 +228,45 @@ if(empty($sideAd) && !AVideoPlugin::loadPluginIfEnabled("Chat2")){
                                    <?php echo $liveTitle; ?>
 
                                 </h1>
-                                <div class="col-xs-12 col-sm-12 col-lg-12"><?php echo $video['creator']; ?></div>
+                                <style>
+.btncustom {
+  background-color: #3F3F3F;
+  border: none;
+  color: white;
+  padding: 5px 10px;
+  font-size: 16px;
+  margin-right: 2px;
+  cursor: pointer;
+}
+.btn1 {
+  background-color: #31AB77;
+  border: none;
+  color: white;
+  padding: 5px 10px;
+  font-size: 16px;
+  cursor: pointer;
+}
+/* Darker background on mouse-over */
+.btncustom:hover {
+  background-color: #31AB77;
+}
+.btn1:hover {
+  background-color: #3F3F3F;
+}
+.btn-group>.btn:first-child:not(:last-child):not(.dropdown-toggle){
+    background-color:#31AB77;
+}
+.btn-group>.btn:last-child:not(:first-child), .btn-group>.dropdown-toggle:not(:first-child){
+    background-color:#31AB77;
+
+}
+</style>
+                                <div class="col-xs-4 col-md-5 col-sm-4 col-lg-7" ><?php echo $video['creator']; ?></div>
+                                <div class="col-xs-3 col-md-3 col-sm-3 col-lg-2" >
+                                <button class="btn btn-xs" style="float: right; background-color:red;">Subscribe</button>
+                                </div>
+                                <div class="col-xs-5 col-md-4 col-sm-5 col-lg-3" >
+                                <?php echo $subscribe; ?></div>
                                 <p><?php echo nl2br(textToLink($liveDescription)); ?></p>
                                 <div class="row">
                                     <div class="col-md-12 watch8-action-buttons text-muted">
@@ -216,6 +321,5 @@ if(empty($sideAd) && !AVideoPlugin::loadPluginIfEnabled("Chat2")){
 </html>
 
 <?php
-//comment
 include $global['systemRootPath'] . 'objects/include_end.php';
 ?>
