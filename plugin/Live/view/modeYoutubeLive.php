f562dd00752bb240973b0a374fe1c36280e15f59
faizy <faizymughal52@gmail.com>
2021-01-05 18:09:48 +0500

    border-radius comment

--- plugin/Live/view/modeYoutubeLive.php
+++ plugin/Live/view/modeYoutubeLive.php
@@ -116,7 +116,7 @@ $poster = Live::getPosterImage($livet['users_id'], $_REQUEST['live_servers_id'])
 ?>
 <link href="<?php echo $global['webSiteRootURL']; ?>plugin/Live/view/live.css" rel="stylesheet" type="text/css"/>
 <div class="row main-video" id="mvideo">
-    <div class="secC col-sm-8 col-md-9">
+    <div class="secC col-sm-8 col-md-9" style="padding-right:1px;padding-left:1px;">
         <div id="videoContainer">
             <div id="floatButtons" style="display: none;">
                 <p class="btn btn-outline btn-xs move">
