adea23379e00020a049d4e2bf216b321fa17f637
faizy <faizymughal52@gmail.com>
2021-01-06 19:27:16 +0500

    share button

--- plugin/Live/view/modeYoutubeLive.php
+++ plugin/Live/view/modeYoutubeLive.php
@@ -267,22 +267,12 @@ echo PlayerSkins::getStartPlayerJS();
 
 }
 </style>
-                                <div class="col-xs-4 col-md-5 col-sm-4 col-lg-5" ><?php echo $video['creator']; ?></div>
-                                <div class="col-xs-3 col-md-3 col-sm-3 col-lg-2" >
-                                <button class="btn btn-m" style="float: right; background-color:#3F3F3F;"><i class="fas fa-check-circle"></i>Subscribe</button>
-                                </div>
-                                <div class="col-xs-3 col-md-3 col-sm-3 col-lg-2" >
-                                <button class="btn btn-m" style="float: right; background-color:#3F3F3F;"><i class="fas fa-check-circle"></i>Subscribe</button>
-                                </div>
-                                <div class="col-xs-5 col-md-4 col-sm-5 col-lg-3" >
-                                <?php echo $subscribe; ?></div>
-                                <p><?php echo nl2br(textToLink($liveDescription)); ?></p>
-                                <div class="row">
-                                    <div class="col-md-12 watch8-action-buttons text-muted">
-                                        <a href="#" class="btn btn-default no-outline" id="shareBtn">
-                                            <span class="fa fa-share"></span> <?php echo __("Share"); ?>
-                                        </a>
-                                        <script>
+                                <div class="col-xs-4 col-md-5 col-sm-4 col-lg-7" ><?php echo $video['creator']; ?></div>
+                                <div class="col-xs-3 col-md-3 col-sm-3 col-lg-5" >
+                         <a href="#" class="btn btn-default no-outline" id="shareBtn">
+                                                                                  
+                         <i class="fas fa-share-alt"></i>
+                            <script>
                                             $(document).ready(function () {
                                                 $("#shareDiv").slideUp();
                                                 $("#shareBtn").click(function () {
@@ -292,6 +282,16 @@ echo PlayerSkins::getStartPlayerJS();
                                                 });
                                             });
                                         </script>
+                                        </a>
+                                        <button class="btn btn-m" style=" background-color:#3F3F3F;"><i class="fas fa-check-circle"></i>Subscribe</button>
+                                        <?php echo $subscribe; ?>
+                                </div>
+                               
+                                <p><?php echo nl2br(textToLink($liveDescription)); ?></p>
+                                <div class="row">
+                                    <div class="col-md-12 watch8-action-buttons text-muted">
+                                      
+                                        
                                         <?php echo AVideoPlugin::getWatchActionButton(0); ?>
                                     </div>
                                 </div>
