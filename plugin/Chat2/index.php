f887e02a0eff57c0041186f55c44dba9f52f773c
faizy <faizymughal52@gmail.com>
2021-01-08 03:07:01 +0500

    inbut message box

--- plugin/Chat2/index.php
+++ plugin/Chat2/index.php
@@ -490,13 +490,14 @@ echo "var credentialsE = '&{$credentials}';";
                                 <div class="row">
                                     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" id="divChatInput">
                                         <input id="chatInput" maxlength="<?php echo $obj->charLimit; ?>" style="display: none;">
+                                        <button class="btn btn-block " id="submitChat">
+                                            <i class="fas fa-paper-plane"></i>
+                                        </button>
                                     </div>
                                     <div class="col-lg-10 col-md-10 col-sm-10 col-xs-10" id="divChatInput">
                                     </div>
                                     <div class=" col-lg-2 col-md-2 col-sm-2 col-xs-2" id="divSubmitChat">
-                                        <button class="btn btn-block " id="submitChat">
-                                            <i class="fas fa-paper-plane"></i>
-                                        </button>
+                                       
                                     </div>
                                 </div>
 
