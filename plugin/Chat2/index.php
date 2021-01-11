5e1709529b4ea5ee33089962155e2f858835b021
faizy <faizymughal52@gmail.com>
2021-01-11 18:57:30 +0500

    chat bubble

--- plugin/Chat2/index.php
+++ plugin/Chat2/index.php
@@ -525,11 +525,7 @@ echo "var credentialsE = '&{$credentials}';";
                                             Send
                                         </button>
                                     </div>
-                                    <div class="col-lg-10 col-md-10 col-sm-10 col-xs-10" id="divChatInput">
-                                    </div>
-                                    <div class=" col-lg-2 col-md-2 col-sm-2 col-xs-2" id="divSubmitChat">
-
-                                    </div>
+                                   
                                 </div>
 
                                 <?php
