02ec280c4c93811b559ac672585cf48286d69584
faizy <faizymughal52@gmail.com>
2021-01-11 21:43:51 +0500

    adding scrollbaar

--- plugin/Chat2/index.php
+++ plugin/Chat2/index.php
@@ -271,6 +271,24 @@ echo "var credentialsE = '&{$credentials}';";
                 font-size:16px;
 
             }
+            ::-webkit-scrollbar {
+  width: 10px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f1f1f1; 
+}
+ 
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #888; 
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #555; 
+}
             .emojionearea.emojionearea-inline>.emojionearea-editor{
                 color:white;
             }
